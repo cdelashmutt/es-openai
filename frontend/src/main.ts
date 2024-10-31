047283603e695f680e29c5d8fb6729af6eedd9e7
Chris DeLashmutt <grog@grogscave.net>
2024-10-31 17:43:51 -0400

    Login

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,20 @@
+import { createAuth0 } from '@auth0/auth0-vue';
+
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(
+  createAuth0({
+    domain: 'dev-qwxg0cfmxcub4nih.us.auth0.com',
+    clientId: 'wqIfAjUc5xNj8P1fjc5n0U53ZmvZSCb5',
+    authorizationParams: {
+      redirect_uri: window.location.origin
+    }
+  })
+);
+
+app.mount('#app');
+
