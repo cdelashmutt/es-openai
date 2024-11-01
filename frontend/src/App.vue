e45189ad8e596368b72fef16a9eeb33e5f2b2fe1
Chris DeLashmutt <grog@grogscave.net>
2024-11-01 11:03:48 -0400

    Better login/logout

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,29 @@
 <template>
-  <Login :isAuthenticated="isAuthenticated"/>
+  <div v-if="isLoading">Loading...</div>
+  <div v-else>
+    <div class="logout" v-if="isAuthenticated">
+      <User :user="user" /><button @click="doLogout">Log out</button>
+    </div>
+    <div v-else>
+      <button @click="doLogin">Log in</button>
+    </div>
+  </div>
 </template>
 <script setup>
-  import { ref } from 'vue';
-  import Login from './components/Login.vue'
+  import { useAuth0 } from '@auth0/auth0-vue';
+  import User from './components/User.vue';
+  const { loginWithRedirect, logout, user, isAuthenticated, isLoading } = useAuth0();
 
-  const isAuthenticated=ref(false);
-
+  const doLogin = () => { 
+    loginWithRedirect();
+  };
+  const doLogout = () => { 
+    logout({ logoutParams: { returnTo: window.location.origin } });
+  }
 </script>
+<style>
+.logout {
+  display: flex;
+  flex-direction: row;
+}
+</style>
---
+++ frontend/src/components/User.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="user-image">
+    <div class="user-image-frame"><img :src="user.picture" /></div>
+    <div class="name">{{ user.name }}</div>
+  </div>
+</template>
+<script setup lang="ts">
+defineProps<{user?: object}>();
+
+</script>
+<style>
+.user-image {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+.user-image-frame {
+  border: 2px solid #c7b89e;
+
+}
+.user-image-frame, .user-image-frame img {
+  width: 50px;
+  height: 50px;
+  border-radius: 30px;
+}
+.name {
+  margin: 5px;
+}
+</style>
